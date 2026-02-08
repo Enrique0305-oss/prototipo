aa2889ff5b540f118c1ef37713e00f39e4cf3f38
Seyda <seyda.rivasher@gmail.com>
2026-02-08 04:24:46 -0500

    plantilla cotizacion PDF

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Cotizacion;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/cotizacion/test/{id}', function($id) {
+    $cotizacion = Cotizacion::with(['cliente', 'detalles.servicio'])->find($id);
+
+    if (!$cotizacion) {
+        return "No se encontró la cotización con ID: $id";
+    }
+
+    return view('CotizacionPDF', compact('cotizacion'));
 });
